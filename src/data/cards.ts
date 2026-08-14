6fa79994d308fa5aed02628f2cdb926c59920681
Drago <dragoidc3@gmail.com>
2026-08-14 18:35:00 -0400

    Load new Expansion cards

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -4,6 +4,7 @@ import batch3 from './cards-3.json'
 import batch4 from './cards-4.json'
 import batch5 from './cards-5.json'
 import batch6 from './cards-6.json'
+import batch7 from './cards-7.json'
 import type { CardDefinition } from '../types'
 
 export const cards = [
@@ -13,6 +14,7 @@ export const cards = [
   ...batch4,
   ...batch5,
   ...batch6,
+  ...batch7,
 ] as CardDefinition[]
 
 export default cards
